f3b60e3fed49b043ac2663a51f1f74e54002f387
mpanelo02 <markjohnsonpanelo.fin@gmail.com>
2025-09-28 16:55:04 +0300

    Add files via upload

--- server.js
+++ server.js
@@ -11,20 +11,31 @@ const ARANET_API_KEY = process.env.ARANET_API_KEY;
 const SIGROW_API_KEY = process.env.SIGROW_API_KEY; // Consider moving this to environment variables too
 const WEATHER_API_KEY = process.env.WEATHER_API_KEY;
 
-// Update the CORS configuration at the top of your server.js
+// CORS configuration
 app.use(cors({
     origin: [
         'http://127.0.0.1:5500', 
         'http://localhost:3000', 
         'http://localhost:5500',
         'https://strawberries-git-main-marks-projects-07a4f883.vercel.app',
-        'https://u-farm-lab-git-main-marks-projects-07a4f883.vercel.app/',
-        'https://simple-hauz-git-main-marks-projects-07a4f883.vercel.app/'
+        'https://u-farm-lab-git-main-marks-projects-07a4f883.vercel.app',
+        'https://simple-hauz-git-main-marks-projects-07a4f883.vercel.app'
         // 'https://strawberries-*.vercel.app'
 
     ],
     credentials: true
 }));
+
+// Middleware
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+  next();
+});
+
+
+app.use(bodyParser.json());
 
 // Remove the individual CORS headers for the weather endpoint
 app.get('/api/weather', async (req, res) => {
